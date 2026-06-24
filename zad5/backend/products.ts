3aaefcf6136105be22c6d9e5f3275216dca9aa65
ratman <szczuras7@proton.me>
2026-06-24 14:28:48 +0200

    pokazowy commit

--- zad5/backend/products.ts
+++ zad5/backend/products.ts
@@ -1,5 +1,5 @@
 export const products = [
-    { id: 1, name: "Dziadek do orzechów", price: 29.99 },
-    { id: 2, name: "Lego Ninjago", price: 49.99 },
-    { id: 3, name: "Buty na rzepy", price: 99.99 },
+  { id: 1, name: "Dziadek do orzechów", price: 29.99 },
+  { id: 2, name: "Lego Ninjago", price: 49.99 },
+  { id: 3, name: "Buty na rzepy", price: 99.99 },
 ];
---
+++ zad6/README.md
@@ -0,0 +1,1 @@
+Zmiany widoczne są w folderze dla zadania 5, commit nazywa się "cleanup + zad6 3.0"
