36cfb02c86820779b1f5116f12a861eec4683efa
ratman <szczuras7@proton.me>
2026-06-24 14:42:10 +0200

    za6 finish eslint and husky config

---
+++ zad5/eslint.config.mjs
@@ -0,0 +1,9 @@
+// domyślna konfiguracja ESLint dla TypeScript
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+);
