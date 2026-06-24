d4269ce2b639990985a4f12ed0459c2b1b5bfdc4
ratman <szczuras7@proton.me>
2026-06-24 14:42:34 +0200

    test husky

--- zad5/backend/products.ts
+++ zad5/backend/products.ts
@@ -1,7 +1,7 @@
 export const products = [
-  { id: 1, name: "Dziadek do orzechów", price: 29.99 },
-  { id: 2, name: "Lego Ninjago", price: 49.99 },
-  { id: 3, name: "Buty na rzepy", price: 99.99 },
-];
+    { id: 1, name: 'Dziadek do orzechów', price: 29.99 },
+    { id: 2, name: 'Lego Ninjago', price: 49.99 },
+    { id: 3, name: 'Buty na rzepy', price: 99.99 },
+]
 
-const unused = "unused";
+const unused = 'unused'
